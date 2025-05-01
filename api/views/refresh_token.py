955478c47ce0967bd533ed24a4d4f5e7190fc214
Jean-Luck Sithi <jean-lucksithi@MacBook-Air-de-Jean-Luck.local>
2025-05-01 21:35:16 +0200

    import inutile

--- api/views/refresh_token.py
+++ api/views/refresh_token.py
@@ -3,7 +3,6 @@ from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework.response import Response
 from datetime import datetime
-from rest_framework.views import APIView
 
 class RefreshToken(TokenRefreshView):
     """
