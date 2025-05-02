f6b42837f8eb5c68354a942e8a2b91a9dd038b83
Jean-Luck Sithi <jean-lucksithi@MacBook-Air-de-Jean-Luck.local>
2025-05-02 15:09:27 +0200

    sauvegarde

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,6 +1,7 @@
 from .partner import Partner
 from .attribute import Attribute
 from .attribute_choice import AttributeChoice
+from .profile import Profile
 
 __all__ = [
     'Partner',
---
+++ api/views/profile.py
@@ -0,0 +1,6 @@
+from rest_framework.views import ViewSet
+
+class ProfileViewSet(ViewSet):
+    
+    def create(self, request, *args, **kwargs):
+        pass
