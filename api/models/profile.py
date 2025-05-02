5e641c41fcd227735eea7e297b6bb6e185c277a8
Jean-Luck Sithi <jean-lucksithi@MacBook-Air-de-Jean-Luck.local>
2025-05-02 14:57:21 +0200

    ajout modèle profile et attribut de profile

---
+++ api/models/profile.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from .partner import Partner
+
+PROFILE_STATUS_CHOICE = [
+    ('sketch', 'Brouillon'),
+    ('pending', 'En attente'),
+    ('complete', 'Complet'),
+    ('in analysis', 'En analyse'),
+    ('analyzed', 'Analysé'),
+]
+
+class Profile(models.Model):
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    partner = models.ForeignKey(Partner, on_delete=models.CASCADE)
+    status = models.CharField(choices=PROFILE_STATUS_CHOICE)
+    externalReference = models.CharField(null=True, blank=True)
---
+++ api/models/profile_attribute.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from .profile import Profile
+from .attribute import Attribute
+
+class ProfileAttribute(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    value = models.CharField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    source = models.CharField(null=True, blank=True) 
