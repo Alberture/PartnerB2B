178d8ca937c120691be413e4d9d48c8cd7066a19
Jean-Luck Sithi <jean-lucksithi@MacBook-Air-de-Jean-Luck.local>
2025-05-16 11:19:34 +0200

    déplacement des fonctions car trop spécifiques

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -1,11 +1,8 @@
 from .responses import valid_response, error_response_template
-from .functions import value_is_between, value_is_in_attribute_choice_set_or_error, check_unique_multiple_choices_validation
+from .functions import value_is_between 
 
 __all__ = [
     'valid_response',
     'error_response_template',
     'value_is_between',
-    'value_is_in_attribute_choice_set_or_error',
-    'check_unique_multiple_choices_validation',
-    'check_unique_multiple_choices_validation'
 ]
